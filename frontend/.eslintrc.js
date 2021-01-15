cf013d1a2d910898eeaa2f126accaf5e2da52d8e
t-kimura <t-kimura@edirium.co.jp>
2021-01-16 00:08:47 +0900

    Add eslint settings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,61 @@
 module.exports = {
   'env': {
-    'browser': true,
-    'es2021': true,
+    'es6': true,
   },
   'extends': [
-    'google',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parser': '@typescript-eslint/parser',
   'parserOptions': {
-    'ecmaVersion': 12,
+    'ecmaVersion': 2018,
     'sourceType': 'module',
   },
+  'plugins': [
+    '@typescript-eslint',
+    'import',
+    'simple-import-sort',
+  ],
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'moduleDirectory': [
+          'node_modules',
+          'src',
+        ]
+      }
+    }
+  },
   'rules': {
-    'no-console': 'warn',
+    'simple-import-sort/exports': 'error',
+    'sort-imports': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/member-ordering': ['warn', {
+      'default': [
+        'static-field',
+        'instance-field',
+        'static-method',
+        'instance-method',
+      ]
+    }],
+    'no-param-reassign': 'error',
+    'no-console': 'error',
+    'no-fallthrough': 'error',
   },
 };
---
+++ frontend/src/main/main.ts
@@ -0,0 +1,5 @@
+import { EventEmitter } from 'events';
+
+export function Foo(a: string): void {
+  console.log(a);
+}
